3353326f3ca7294b0635ce10c6322a0719e233ac
Philipp Siedler <p.d.siedler@gmail.com>
2017-12-05 13:01:49 +0000

    adding ch16e2

--- p578_2_checkerboard_square_buttons.cpp
+++ p578_2_checkerboard_square_buttons.cpp
@@ -62,33 +62,74 @@ struct cheker_board_buttons : My_window
 	cheker_board_buttons(Point xy, int w, int h, const string& title)
 		:My_window(xy, w, h, title)
 	{
-		int j = 0;
-		for (int i = 0; i < 16; i++)
-		{
-			if (i % 4 == 0 && i != 0) { j++; }
+		int x_size = xmax / 4;
+		int y_size = ymax / 4;
 
-			pressed.push_back(false);
-			string label = "b";
-			label += i;
-			Button b(Point((xmax / 4) * (i % 4 + 1) - (xmax / 4), j * (ymax / 4) + 20), xmax / 4, ymax / 4, label, cb_change);
-			buttons.push_back(b);
-			attach(b);
-		}
+		// initialize buttons
+		buttons.push_back(new Button(Point(0, 20), xmax / 4, ymax / 4, "b0", cb_change_0));
+		buttons.push_back(new Button(Point(x_size, 20), xmax / 4, ymax / 4, "b1", cb_change_1));
+		buttons.push_back(new Button(Point(x_size * 2, 20), xmax / 4, ymax / 4, "b2", cb_change_2));
+		buttons.push_back(new Button(Point(x_size * 3, 20), xmax / 4, ymax / 4, "b3", cb_change_3));
+		buttons.push_back(new Button(Point(0, y_size + 20), xmax / 4, ymax / 4, "b4", cb_change_4));
+		buttons.push_back(new Button(Point(x_size, y_size + 20), xmax / 4, ymax / 4, "b5", cb_change_5));
+		buttons.push_back(new Button(Point(x_size * 2, y_size + 20), xmax / 4, ymax / 4, "b6", cb_change_6));
+		buttons.push_back(new Button(Point(x_size * 3, y_size + 20), xmax / 4, ymax / 4, "b7", cb_change_7));
+		buttons.push_back(new Button(Point(0, y_size * 2 + 20), xmax / 4, ymax / 4, "b8", cb_change_8));
+		buttons.push_back(new Button(Point(x_size, y_size * 2 + 20), xmax / 4, ymax / 4, "b9", cb_change_9));
+		buttons.push_back(new Button(Point(x_size * 2, y_size * 2 + 20), xmax / 4, ymax / 4, "b10", cb_change_10));
+		buttons.push_back(new Button(Point(x_size * 3, y_size * 2 + 20), xmax / 4, ymax / 4, "b11", cb_change_11));
+		buttons.push_back(new Button(Point(0, y_size * 3 + 20), xmax / 4, ymax / 4, "b12", cb_change_12));
+		buttons.push_back(new Button(Point(x_size, y_size * 3 + 20), xmax / 4, ymax / 4, "b13", cb_change_13));
+		buttons.push_back(new Button(Point(x_size * 2, y_size * 3 + 20), xmax / 4, ymax / 4, "b14", cb_change_14));
+		buttons.push_back(new Button(Point(x_size * 3, y_size * 3 + 20), xmax / 4, ymax / 4, "b15", cb_change_15));
+
+		// attach buttons
+		for (int i = 0; i < buttons.size(); i++) { attach(buttons[i]); }
 	}
 
-	
+	// button setup
 	Vector_ref<Button> buttons;
-	void change() { hide(); };
+	int last = 16;
+	void reset_button() { if(last != 16) { buttons[last].show(); } }	
+
+	// button behaviour
+	void change_b0() { reset_button(); last = 0; buttons[0].hide(); };
+	void change_b1() { reset_button(); last = 1; buttons[1].hide(); };
+	void change_b2() { reset_button(); last = 2; buttons[2].hide(); };
+	void change_b3() { reset_button(); last = 3; buttons[3].hide(); };
+	void change_b4() { reset_button(); last = 4; buttons[4].hide(); };
+	void change_b5() { reset_button(); last = 5; buttons[5].hide(); };
+	void change_b6() { reset_button(); last = 6; buttons[6].hide(); };
+	void change_b7() { reset_button(); last = 7; buttons[7].hide(); };
+	void change_b8() { reset_button(); last = 8; buttons[8].hide(); };
+	void change_b9() { reset_button(); last = 9; buttons[9].hide(); };
+	void change_b10() { reset_button(); last = 10; buttons[10].hide(); };
+	void change_b11() { reset_button(); last = 11; buttons[11].hide(); };
+	void change_b12() { reset_button(); last = 12; buttons[12].hide(); };
+	void change_b13() { reset_button(); last = 13; buttons[13].hide(); };
+	void change_b14() { reset_button(); last = 14; buttons[14].hide(); };
+	void change_b15() { reset_button(); last = 15; buttons[15].hide(); };
 
 private:
 	vector<bool> pressed;
 
-	static void cb_change(Address, Address addr) // callback for quit_button
-											   //	{ reference_to<Simple_window>(addr).quit(); }
-	{
-		static_cast<cheker_board_buttons*>(addr)->change();
-	}
-
+	// call back functions
+	static void cb_change_0(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b0(); }
+	static void cb_change_1(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b1(); }
+	static void cb_change_2(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b2(); }
+	static void cb_change_3(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b3(); }
+	static void cb_change_4(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b4(); }
+	static void cb_change_5(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b5(); }
+	static void cb_change_6(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b6(); }
+	static void cb_change_7(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b7(); }
+	static void cb_change_8(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b8(); }
+	static void cb_change_9(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b9(); }
+	static void cb_change_10(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b10(); }
+	static void cb_change_11(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b11(); }
+	static void cb_change_12(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b12(); }
+	static void cb_change_13(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b13(); }
+	static void cb_change_14(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b14(); }
+	static void cb_change_15(Address, Address addr) { reference_to<cheker_board_buttons>(addr).change_b15(); }
 };
 
 int main()
