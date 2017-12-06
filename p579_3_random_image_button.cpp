2cc5a37fbd74e55bec4cf3f862bca97c6cbdac7d
Philipp Siedler <p.d.siedler@gmail.com>
2017-12-06 14:07:13 +0000

    finishing ch16e3 and adding Data/supercell.jpg

--- p579_3_random_image_button.cpp
+++ p579_3_random_image_button.cpp
@@ -59,12 +59,50 @@ private:
 	void quit() { hide(); }
 };
 
+struct Random_button_win : My_window
+{
+	Random_button_win(Point xy, int w, int h, const string& title, const string& url)
+		:My_window(xy, w, h, title),
+		url(url),
+		rand_button(Point(xmax/2, ymax/2), 40, 40, "", cb_rand),
+		button_image(Point(xmax / 2, ymax / 2), url)
+	{
+		attach(rand_button);
+		button_image.set_mask(Point(xmax / 2, ymax / 2), 40, 40);
+		attach(button_image);
+	}
+		
+	Button rand_button;
+	Image button_image;
+	
+private:
+	string url;
 
+	static void cb_rand(Address, Address addr)
+	{
+		static_cast<Random_button_win*>(addr)->change_button_pos();
+	}
+
+	void change_button_pos()
+	{
+		rand_button.hide();
+
+		int current_x = rand_button.loc.x;
+		int current_y = rand_button.loc.y;
+
+		int new_x = rand_int(0, xmax - 40) - current_x;
+		int new_y = rand_int(20, ymax - 40) - current_y;
+		
+		rand_button.move(new_x, new_y);
+		button_image.move(new_x, new_y);
+		redraw();
+	}
+};
 
 int main()
 try
 {
-	//cheker_board_buttons win(Point(100, 100), xmax, ymax + 20, "checker board buttons");
+	Random_button_win win(Point(100, 100), xmax, ymax + 20, "random buttons window", "Data/supercell.jpg");
 	return gui_main();
 }
 catch (exception& e) {
@@ -72,7 +110,6 @@ catch (exception& e) {
 	keep_window_open();
 	return 1;
 }
-
 catch (...) {
 	cerr << "Unknown exception!\n";
 	keep_window_open();
